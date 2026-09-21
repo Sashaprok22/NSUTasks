fc75e4b93b0c5daac46a0a6704c8c984bb084f78
Sashaprok <sashaprok22@gmail.com>
2026-09-21 15:48:02 +0700

    ML: Pack 2 микро правки

--- MachineLearning/packs/2/ProkudinAlexandr1.py
+++ MachineLearning/packs/2/ProkudinAlexandr1.py
@@ -1,5 +1,5 @@
 def is_palindrome(s):
     return s == s[::-1]
 
-text = input()
+text = input().lower()
 print("YES" if is_palindrome(text) else "NO")
--- MachineLearning/packs/2/ProkudinAlexandr2.py
+++ MachineLearning/packs/2/ProkudinAlexandr2.py
@@ -1,6 +1,7 @@
 nums = list(map(int, input().split()))
 direction = input()
 positions = int(input())
+positions %= len(nums)
 positions = -positions if direction == "R" else positions
 new_nums = nums[positions:] + nums[:positions]
 print(new_nums)
